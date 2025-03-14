8ae049dfe3b5cb5e4c688794ca4d075080596e9a
Eryko <eryko4350@gmail.com>
2025-03-14 17:25:34 -0300

    pipeline test

--- airflow1.py
+++ airflow1.py
@@ -1,1 +1,22 @@
-print("Hello World")
+from time import sleep
+
+def primeira_atividade():
+    print('Primeira atividade')
+    sleep(2)
+    
+def segunda_atividade():
+    print('Segunda atividade')
+    sleep(2)  
+    
+def terceira_atividade():
+    print('Terceira atividade')
+    sleep(2)
+    
+def pipeline():
+    primeira_atividade()
+    segunda_atividade()
+    terceira_atividade()
+    print('Pipeline finalizada')
+    
+if __name__ == '__main__':
+    pipeline()
